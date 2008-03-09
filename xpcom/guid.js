7eab0034fc4b68c26e352232f2dd91d90e898082
marikom <marikom@21796ef0-4f62-4552-8d1b-68394bc7e1f1>
2008-03-09 06:08:21 +0000

    -changed the extension packager's guid generator implementation from java to javascript
    -this change causes the extension packager to work up until the point where it calls down into
    java to do the actual putting together of the .xpi file
    -previously was crashing as soon as dialog opened, because of java implementation of guid generator
    
    
    git-svn-id: https://svn.csail.mit.edu/chickenfoot/trunk/chickenfoot@1370 21796ef0-4f62-4552-8d1b-68394bc7e1f1

--- xpcom/guid.js
+++ xpcom/guid.js
@@ -1,12 +1,10 @@
 
 /**
  * Creates a new, random GUID
+ *
+ * NOTE: Found this code on the web at http://note19.com/2007/05/27/javascript-guid-generator/
  */
 function generateRandomGuid() {
-  var guidClass = getJavaClass("chickenfoot.RandomGUID");
-  var guidCreateFunc = guidClass.getMethod("createRandomGUID", []);
-  var guid = guidCreateFunc.invoke(null, [ ]);
-
-  // make sure guid is a JS string instead of a Java string
-  return ("" + guid.toString());
+  function randNum() { return (((1+Math.random())*0x10000)|0).toString(16).substring(1); }
+  return "" + (randNum()+randNum()+"-"+randNum()+"-"+randNum()+"-"+randNum()+"-"+randNum()+randNum()+randNum());
 }
