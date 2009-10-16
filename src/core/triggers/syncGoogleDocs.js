e152e9f037e0935343b85718c4160b7b0f1a0d51
mbolin <mbolin@21796ef0-4f62-4552-8d1b-68394bc7e1f1>
2009-10-16 23:07:38 +0000

    Updated syncGoogleDocs.js, fixing some style issues and eliminating some extra function params.
    git-svn-id: https://svn.csail.mit.edu/chickenfoot/trunk/chickenfoot@2693 21796ef0-4f62-4552-8d1b-68394bc7e1f1

--- src/core/triggers/syncGoogleDocs.js
+++ src/core/triggers/syncGoogleDocs.js
@@ -1,103 +1,124 @@
-/*
- * Functions for syncing via Google Docs
+/**
+ * @fileoverview Functions for syncing via Google Docs.
  */
 
-// get auth value for google docs
-function getGDocsAuth(email, password)
-{
-  var auth;
-  var goog_url = "https://www.google.com/accounts/ClientLogin";
+
+/**
+ * Gets a GData Google Docs auth token to use with the Google Account that
+ * corresponds to the specified email address. Uses Google's ClientLogin to
+ * obtain the token:
+ * http://code.google.com/apis/accounts/docs/AuthForInstalledApps.html
+ * @param {string} email Email address of the account to get an auth token for.
+ * @param {string} password Password for the account.
+ * @return {string?} The auth token
+ */
+function getGDocsAuth(email, password) {
+  var authToken = null;
+  var googUrl = "https://www.google.com/accounts/ClientLogin";
   var request = new XMLHttpRequest();
   var asynchronous = false;
-  request.open("POST", goog_url, asynchronous);
+  request.open("POST", googUrl, asynchronous);
 
-  var params = "accountType=GOOGLE&Email=" + email + "&Passwd=" + password + "&service=writely";
+  var params = "accountType=GOOGLE&Email=" + encodeURIComponent(email) +
+      "&Passwd=" + encodeURIComponent(password) + "&service=writely";
   request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   request.setRequestHeader("GData-Version", "2.0");
   request.setRequestHeader("Connection", "close");
   request.send(params);
 
   if (request.status == 200) {
-   data = request.responseText;
-   auth = data.split("\n")[2].substring(5)
+    var data = request.responseText;
+    var lines = data.split('\n');
+    for (var i = 0; i < lines.length; ++i) {
+      var line = lines[i];
+      var match = line.match(/^Auth=([\w-]+)$/);
+      if (match) {
+        authToken = match[1];
+        break;
+      }
+    }
   } else {
-   throw new Error('Login error: ' + request.status + ' ' +
-  request.statusText);
+    throw new Error('Login error: ' + request.status + ' ' +
+        request.statusText);
   }
-  return auth;
+  return authToken;
 }
 
-// make a request to GDocs
-function getGDocsXMLfromURL(auth, URL, protocol, thingToSend)
-{
+
+/**
+ * Makes an authenticated GET request to GDocs.
+ * @param {string} authToken Token acquired using getGDocsAuth().
+ * @param {string} url GDocs URL to make the request to.
+ * @return {string} The text of the response from the GDocs server.
+ */
+function getGDocsXMLfromURL(authToken, url) {
   var request = new XMLHttpRequest();
   var asynchronous = false;
-  request.open(protocol, URL, asynchronous);
-  
-  request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  request.open("GET", url, asynchronous);
+
   request.setRequestHeader("GData-Version", "2.0");
-  request.setRequestHeader("Authorization", "GoogleLogin auth=" +auth);
+  request.setRequestHeader("Authorization", "GoogleLogin auth=" + authToken);
   request.setRequestHeader("Connection", "close");
-  request.send(thingToSend);
+  request.send(null);
 
-  if (request.status == 200) 
-  {
-     var data = request.responseText;
-     return data;
-  }
-  else
-  {
-   throw new Error('Get list: read error: ' + request.status + ' ' + request.statusText);
+  if (request.status == 200) {
+    return request.responseText;
+  } else {
+    throw new Error('Error making GET request: ' + request.status + ' ' +
+        request.statusText);
   }
 }
 
-// make a request to GDocs by sending xml
-function getGDocsXMLfromURL_sendXML(auth, URL, protocol, thingToSend)
-{
+
+/**
+ * Makes an authenticated POST request to GDocs that sends XML.
+ * @param {string} authToken Token acquired using getGDocsAuth().
+ * @param {string} url GDocs URL to make the request to.
+ * @param {string} xml
+ * @return {string}
+ */
+function getGDocsXMLfromURL_sendXML(authToken, url, protocol, xml) {
   var request = new XMLHttpRequest();
   var asynchronous = false;
-  request.open(protocol, URL, asynchronous);
-  
+  request.open(protocol, url, asynchronous);
+
   request.setRequestHeader("Content-Type", "application/atom+xml");
   request.setRequestHeader("GData-Version", "2.0");
-  request.setRequestHeader("Authorization", "GoogleLogin auth=" +auth);
-  request.setRequestHeader("Content-Length", thingToSend.length);
+  request.setRequestHeader("Authorization", "GoogleLogin auth=" + authToken);
+  request.setRequestHeader("Content-Length", xml.length);
   request.setRequestHeader("Connection", "close");
-  request.send(thingToSend);
+  request.send(xml);
 
-  if (request.status == 201)
-  {
-     var data = request.responseText;
-     return data;
-  }
-  else 
-  {
-   throw new Error('ERROR: ' + request.status + ' ' +
-   request.statusText);
+  if (request.status == 201) {
+    var data = request.responseText;
+    return data;
+  } else {
+    throw new Error('ERROR: ' + request.status + ' ' +
+        request.statusText);
   }
 }
 
+
 function _constructContentBody(docTitle, docType, contentBody, contentType) {
-  var atom = ["<?xml version='1.0' encoding='UTF-8'?>",  
-              '<entry xmlns="http://www.w3.org/2005/Atom">',  
-              '<category scheme="http://schemas.google.com/g/2005#kind"',  
-              ' term="http://schemas.google.com/docs/2007#', docType, '" label="', docType, '"/>',  
-              '<title>', docTitle, '</title>',  
-              '</entry>'].join('');  
-               
-  var body = ['--END_OF_PART\r\n',  
-              'Content-Type: application/atom+xml;\r\n\r\n',  
-              atom, '\r\n',  
-              '--END_OF_PART\r\n',  
-              'Content-Type: ', contentType, '\r\n\r\n',  
-              contentBody, '\r\n',  
-              '--END_OF_PART--\r\n'].join('');  
+  var atom = "<?xml version='1.0' encoding='UTF-8'?>" +
+             '<entry xmlns="http://www.w3.org/2005/Atom">' +
+             '<category scheme="http://schemas.google.com/g/2005#kind"' +
+             ' term="http://schemas.google.com/docs/2007#' + docType + '" label="' + docType + '"/>' +
+             '<title>' + docTitle + '</title>' +
+             '</entry>';
+
+  var body = '--END_OF_PART\r\n' +
+             'Content-Type: application/atom+xml;\r\n\r\n' +
+             atom + '\r\n' +
+             '--END_OF_PART\r\n' +
+             'Content-Type: ' + contentType + '\r\n\r\n' +
+             contentBody + '\r\n' +
+             '--END_OF_PART--\r\n';  
   return body;  
 }  
 
 // make a plain text request to GDocs
-function getGDocsXMLfromURL_sendText(auth, URL, protocol, thingToSend)
-{
+function getGDocsXMLfromURL_sendText(auth, URL, protocol, thingToSend) {
   var request = new XMLHttpRequest();
   var asynchronous = false;
   request.open(protocol, URL, asynchronous);
@@ -110,21 +131,17 @@ function getGDocsXMLfromURL_sendText(auth, URL, protocol, thingToSend)
   request.setRequestHeader("Content-Type", "text/plain;");
   request.send(thingToSend);
 
-  if (request.status == 200)
-  {
+  if (request.status == 200) {
      var data = request.responseText;
      return data;
-  }
-  else 
-  {
+  } else {
    throw new Error('ERROR: ' + request.status + ' ' +
    request.statusText);
   }
 } 
 
 // send multipart content
-function getGDocsXMLfromURL_sendMultipart(auth, URL, protocol, thingToSend)
-{
+function getGDocsXMLfromURL_sendMultipart(auth, URL, protocol, thingToSend) {
   var request = new XMLHttpRequest();
   var asynchronous = false;
   request.open(protocol, URL, asynchronous);
@@ -137,17 +154,14 @@ function getGDocsXMLfromURL_sendMultipart(auth, URL, protocol, thingToSend)
   request.setRequestHeader("Content-Type", "multipart/related; boundary=END_OF_PART");
   request.send(thingToSend);
 
-  if (request.status == 200)
-  {
+  if (request.status == 200) {
      var data = request.responseText;
      return data;
-  }
-  else 
-  {
+  } else {
    throw new Error('ERROR: ' + request.status + ' ' +
    request.statusText);
   }
-} 
+}
 
 // make chickenfoot triggers folder
 function createGDocsChickenfootFolder(auth) {
@@ -180,7 +194,7 @@ function createGDocsEmptyFile(auth, folderid, filename) {
 // get chickenfoot triggers folder id
 function getGDocsChickenfootFolderID(auth) {
   var url = "http://docs.google.com/feeds/documents/private/full?showfolders=true";
-  var result = getGDocsXMLfromURL(auth, url, "GET", "");
+  var result = getGDocsXMLfromURL(auth, url);
   
   var id = _findTitleInXML(result, "Chickenfoot Triggers");
   // if not found, create folder
@@ -195,7 +209,7 @@ function getGDocsChickenfootFolderID(auth) {
 // get all file names in chickenfoot folder
 function getGDocsAllChickenfootFileNames(auth, folderid) {
   var url = "http://docs.google.com/feeds/folders/private/full/folder%3A" + folderid;
-  var result = getGDocsXMLfromURL(auth, url, "GET", "");
+  var result = getGDocsXMLfromURL(auth, url);
   
   var titles = new Array();
   var j = 0;
@@ -217,7 +231,7 @@ function getGDocsAllChickenfootFileNames(auth, folderid) {
 // check if folder contains filename
 function containsGDocsChickenfootScript(auth, folderid, filename) {
   var url = "http://docs.google.com/feeds/folders/private/full/folder%3A" + folderid;
-  var result = getGDocsXMLfromURL(auth, url, "GET", "");
+  var result = getGDocsXMLfromURL(auth, url);
   
   var id = _findTitleInXML(result, filename);
   return (id != "");
@@ -226,7 +240,7 @@ function containsGDocsChickenfootScript(auth, folderid, filename) {
 // get document id of a chickenfoot script (create empty if not exist)
 function getGDocsChickenfootScriptID(auth, folderid, filename) {
   var url = "http://docs.google.com/feeds/folders/private/full/folder%3A" + folderid;
-  var result = getGDocsXMLfromURL(auth, url, "GET", "");
+  var result = getGDocsXMLfromURL(auth, url);
   
   var id = _findTitleInXML(result, filename);
   // if not found, create empty file
@@ -241,7 +255,7 @@ function getGDocsChickenfootScriptID(auth, folderid, filename) {
 // get document's edit link for a chickenfoot script (create empty if not exist)
 function getGDocsChickenfootScriptEditLink(auth, folderid, filename) {
   var url = "http://docs.google.com/feeds/folders/private/full/folder%3A" + folderid;
-  var result = getGDocsXMLfromURL(auth, url, "GET", "");
+  var result = getGDocsXMLfromURL(auth, url);
 
   var editlink = _findEditLinkByTitleInXML(result, filename);
   // if not found, create empty file
@@ -301,7 +315,7 @@ function _findEditLinkByTitleInXML(xml, title) {
 function readGDocsDocument(auth, folderid, filename) {
   var documentid = getGDocsChickenfootScriptID(auth, folderid, filename);
   var url = "http://docs.google.com/feeds/download/documents/Export?docID=" + documentid + "&exportFormat=txt";
-  var result = getGDocsXMLfromURL(auth, url, "GET", "");
+  var result = getGDocsXMLfromURL(auth, url);
   
   return result;
 }
